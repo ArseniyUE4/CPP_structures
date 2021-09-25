3823056ed03d04daed7600993aba3a31223d50fe
ArseniyUE4 <91388672+ArseniyUE4@users.noreply.github.com>
2021-09-25 22:07:06 +0300

    ArseniyUE4

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+struct STRUCT1
+{
+	int year;
+	std::string name;
+};
+
+struct STRUCT2 
+{
+	std::string model;
+	STRUCT1 struct1;
+};
+
+int main() {
+	using namespace std;
+
+	STRUCT2 struct2;
+	struct2.model = "Model";
+	struct2.struct1.year = 2001;
+	struct2.struct1.name = "Name";
+
+	cout << struct2.model << endl;
+	cout << struct2.struct1.year << endl;
+	cout << struct2.struct1.name << endl;
+
+}
